42b955f64663a56ba9b0d1e0e705c0f5defd1797
Enzo Giovanni <enzingiovanni@gmail.com>
2021-11-14 20:50:50 -0300

    Envio de todos os arquivos, pq o site ta full funcionando :)

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ $(function() {
 	    // how simpleCart should checkout, see the checkout reference for more info
 	    checkout: {
 	        type: "PayPal" ,
-	        email: "enzingiovanni@gmail.com"
+	        email: "checkout@tadinhos.com.br"
 	    },
 
 	    // set the currency, see the currency reference for more info
